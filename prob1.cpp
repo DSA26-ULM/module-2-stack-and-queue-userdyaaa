ea3a1e2561084f3c5dd978a9c1dc9796f8d2645b
userdyaaa <nadyanadhirsoraya@gmail.com>
2026-04-22 20:37:18 +0800

    Update prob1.cpp

--- prob1.cpp
+++ prob1.cpp
@@ -1,21 +1,10 @@
-#include <iostream>
-#include <string>
-#include <stdexcept>
-#include "stack.h"
-
-using namespace std;
-
 int main() {
-    int n;
-    if (!(cin >> n)) return 0;
-    
     Stack s;
     init(&s); 
     
-    for (int i = 0; i < n; i++) {
-        string simbol;
-        cin >> simbol;
-        
+    string simbol;
+    // Baca terus selama masih ada simbol yang diketik
+    while (cin >> simbol) {
         if (simbol == "+" || simbol == "-" || simbol == "*" || simbol == "/") {
             int b = peek(&s); pop(&s);
             int a = peek(&s); pop(&s);
@@ -30,6 +19,5 @@ int main() {
     }
     
     cout << peek(&s) << endl;
-    
     return 0;
 }
